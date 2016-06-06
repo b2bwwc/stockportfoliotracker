370042046461872307265aad60b8fa4014852c4f
jsp <jspenkar@gmail.com>
2016-06-05 20:28:09 -0500

    After_sign_in_path breaking admin logins.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def after_sign_in_path_for(resource)
+  # def after_sign_in_path_for(resource)
     # return redirect_to (user_path)
-  end
+  # end 
 
   # def after_user_sign_in
   #
