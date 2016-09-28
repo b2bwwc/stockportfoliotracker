7f6edc2251c693ac8227ba7a78cdea8d8c5dbeb3
jsp <jspenkar@gmail.com>
2016-09-27 23:07:05 -0500

    Indexfund artifacts removed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ActiveRecord::Base
-  has_many :indexfunds
   has_many :portfolios
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable       
+         :recoverable, :rememberable, :trackable, :validatable
 end
--- app/views/indexfunds/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@indexfunds) do |indexfund|
-  json.extract! indexfund, :id
-  json.url indexfund_url(indexfund, format: :json)
-end
